24bf7fc473a555fa7f04889e1f63f991c19db32f
ChernykhVyacheslav <chernihvyacheslav@gmail.com>
2021-04-27 12:02:26 +0300

    Created template Spring Boot project

---
+++ src/main/java/com/softserve/borda/BordaApplication.java
@@ -0,0 +1,13 @@
+package com.softserve.borda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BordaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BordaApplication.class, args);
+    }
+
+}

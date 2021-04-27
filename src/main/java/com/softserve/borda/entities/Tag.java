5eb3e073d7517728f4dbad6389e7107cad3d5716
ChernykhVyacheslav <chernihvyacheslav@gmail.com>
2021-04-27 20:01:29 +0300

    Added template version of Tag Entity and basic repositories

---
+++ src/main/java/com/softserve/borda/entities/Tag.java
@@ -0,0 +1,24 @@
+package com.softserve.borda.entities;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+
+@Data
+@Entity
+public class Tag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String description;
+}
---
+++ src/main/java/com/softserve/borda/repositories/TagRepository.java
@@ -0,0 +1,8 @@
+package com.softserve.borda.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface TagRepository extends JpaRepository<Tag, Long> {
+}

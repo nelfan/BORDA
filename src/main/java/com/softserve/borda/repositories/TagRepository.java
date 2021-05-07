8e99cb63da202b0bd16af0ad540f2c112474d2aa
ChernykhVyacheslav <chernihvyacheslav@gmail.com>
2021-05-07 10:53:54 +0300

    Minor fixes

--- src/main/java/com/softserve/borda/repositories/TagRepository.java
+++ src/main/java/com/softserve/borda/repositories/TagRepository.java
@@ -1,11 +1,8 @@
 package com.softserve.borda.repositories;
 
 import com.softserve.borda.entities.Tag;
-import com.softserve.borda.entities.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-import java.util.List;
 
 @RepositoryRestResource
 public interface TagRepository extends JpaRepository<Tag, Long> {

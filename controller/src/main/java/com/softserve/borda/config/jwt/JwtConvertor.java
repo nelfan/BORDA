0f42bc90e674adbbfa373e623c65e9da9c0c5086
ChernykhVyacheslav <chernihvyacheslav@gmail.com>
2021-05-26 12:33:13 +0300

    Divided BORDA application on six modules. Front-end part removed from the back-end side.

---
+++ controller/src/main/java/com/softserve/borda/config/jwt/JwtConvertor.java
@@ -0,0 +1,30 @@
+package com.softserve.borda.config.jwt;
+
+import com.softserve.borda.entities.JWTUser;
+import com.softserve.borda.entities.User;
+import com.softserve.borda.repositories.JWTUserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtConvertor {
+
+    private final com.softserve.borda.config.jwt.JwtProvider jwtProvider;
+
+    private final JWTUserRepository jwtUserRepository;
+
+
+    public JwtConvertor(com.softserve.borda.config.jwt.JwtProvider jwtProvider, JWTUserRepository jwtUserRepository) {
+        this.jwtProvider = jwtProvider;
+        this.jwtUserRepository = jwtUserRepository;
+    }
+
+
+    public String saveUser(User user){
+        jwtUserRepository.save(new JWTUser(jwtProvider.generateToken(user.getUsername()),user));
+        return jwtProvider.generateToken(user.getUsername());
+    }
+
+    public User getUserByJWT(String jwt){
+        return jwtUserRepository.findById(jwt).orElseThrow().getUser();
+    }
+}

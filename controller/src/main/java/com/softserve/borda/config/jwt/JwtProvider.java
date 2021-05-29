96e435113c0ffd6dfb0732e5313a317f62bdcfc5
jules <neroba.yulia@gmail.com>
2021-05-29 19:49:13 +0300

    Removed expiration of token

--- controller/src/main/java/com/softserve/borda/config/jwt/JwtProvider.java
+++ controller/src/main/java/com/softserve/borda/config/jwt/JwtProvider.java
@@ -19,10 +19,8 @@ public class JwtProvider {
     private String jwtSecret;
 
     public String generateToken(String login) {
-        Date date = Date.from(LocalDate.now().plusDays(15).atStartOfDay(ZoneId.systemDefault()).toInstant());
         return Jwts.builder()
                 .setSubject(login)
-                .setExpiration(date)
                 .signWith(SignatureAlgorithm.HS512, jwtSecret)
                 .compact();
     }

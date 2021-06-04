b67ef401e72b8714297376a5d757e71b9a1c9322
nelfan <nelfan95@gmail.com>
2021-06-04 16:16:50 +0300

    Add checkUserDTO

---
+++ domain/src/main/java/com/softserve/borda/dto/CheckUserDTO.java
@@ -0,0 +1,14 @@
+package com.softserve.borda.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CheckUserDTO {
+
+    private String username;
+
+}

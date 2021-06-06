60041b5a9f5da5c4f35fb3a416419cc7d6a963ff
nelfan <nelfan95@gmail.com>
2021-06-07 00:46:17 +0300

    Add handleMethodArgumentNotValid

--- shared/src/main/java/com/softserve/borda/config/RestResponseEntityExceptionHandler.java
+++ shared/src/main/java/com/softserve/borda/config/RestResponseEntityExceptionHandler.java
@@ -2,16 +2,21 @@ package com.softserve.borda.config;
 
 import com.softserve.borda.exceptions.CustomEntityNotFoundException;
 import com.softserve.borda.exceptions.CustomFailedToDeleteEntityException;
-import com.softserve.borda.exceptions.CustomValidationFailedException;
 import lombok.extern.java.Log;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 @Log
@@ -45,12 +50,20 @@ public class RestResponseEntityExceptionHandler
                 new HttpHeaders(), HttpStatus.UNAUTHORIZED, request);
     }
 
-    @ExceptionHandler(value
-            = {CustomValidationFailedException.class})
-    protected ResponseEntity<Object> handleValidationFailed(
-            RuntimeException ex, WebRequest request) {
-        log.warning(ex.getMessage());
-        return handleExceptionInternal(ex, ex.getMessage(),
-                new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+    @Override
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatus status,
+                                                                  WebRequest request) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+            log.warning(fieldName + ": [" + ((FieldError) error).getRejectedValue() + "] is invalid because " + errorMessage);
+        });
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
+
 }
